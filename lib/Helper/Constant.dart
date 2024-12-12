5e3e9c526061e37bad7877541bad8b7b6cbb51e8
Mady938 <forsmmscript1@gmail.com>
2024-12-12 17:54:12 +0530

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+const String appName = 'richzone';
+
+const String packageName = 'com.richzone.app';
+const String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+const String iosPackage = 'com.richzone.app';
+const String iosLink = 'your ios link here';
+const String appStoreId = '123456789';
+
+const String deepLinkUrlPrefix = 'https://eshopwrteamin.page.link';
+const String deepLinkName = 'eshop';
+
+const int timeOut = 50;
+const int perPage = 10;
+
+
+const String baseUrl = 'https://richzoneapp.in/app/v1/api/';
+const String jwtKey = "408c664e506a8488dadea48e2bb2d8b66c758e84";
+
+
+
+
